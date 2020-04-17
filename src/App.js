edba52b09d9badcbf3cd04c6bd57322929e12a67
Herivelton Sousa <heriveltonsp@gmail.com>
2020-04-16 21:01:02 -0300

    Desafio OK

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import api from "./services/api";
+
 import {
   SafeAreaView,
   View,
@@ -9,7 +11,6 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
-import api from "./services/api";
 
 
 export default function App() {
